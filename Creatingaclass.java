6115a87098d1a0571030d046dcb429a463f2f4da
SeanMuthomi <sean.muthomi@strathmore.edu>
2025-04-28 11:47:52 +0300

    Add files via upload

---
+++ Creatingaclass.java
@@ -0,0 +1,14 @@
+public class ShowDetails {
+
+
+    void showDetails() {
+        System.out.println("Name: Sean Muthomi");
+        System.out.println("Admission no.: 190119");
+    }
+
+
+    public static void main(String[]args) {
+        ShowDetails obj = new ShowDetails();
+        obj.showDetails();
+    }
+}
