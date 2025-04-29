8512a148a8bd8efecfce6aa98d9696a95bce4d78
SeanMuthomi <sean.muthomi@strathmore.edu>
2025-04-29 20:49:10 +0300

    Add files via upload

---
+++ Imports_Challnge4.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Imports_Challenge4 {
+    public static void main(String[]args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age= sc.nextInt();
+        System.out.println("You are "+age+" years old.");
+    }
+
+}
+
